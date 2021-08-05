b86a58f010645863052973bb228691d7321b727a
amy gallacher <amy.gallacher@codeclan.com>
2021-08-05 14:52:51 +0100

    adds ability to adjust total cash amount

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -4,3 +4,6 @@ def get_pet_shop_name(shop_details):
 
 def get_total_cash(shop_details):
     return shop_details["admin"]["total_cash"]
+
+def add_or_remove_cash(shop_details, amount):
+    shop_details["admin"]["total_cash"] += amount
