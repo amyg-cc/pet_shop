bd5a7f12719600ddb143df1bdd42ba9c60513ecc
amy gallacher <amy.gallacher@codeclan.com>
2021-08-05 14:56:49 +0100

    allows adjustment of number of pets sold

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -10,3 +10,6 @@ def add_or_remove_cash(shop_details, amount):
 
 def get_pets_sold(shop_details):
     return shop_details["admin"]["pets_sold"]
+
+def increase_pets_sold(shop_details, amount):
+    shop_details["admin"]["pets_sold"] += amount
