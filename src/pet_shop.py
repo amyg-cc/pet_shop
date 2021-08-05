1e7c5c7b2515430d845d4e1cdb908587ee9ba1ba
amy gallacher <amy.gallacher@codeclan.com>
2021-08-05 14:39:50 +0100

    returns total cash

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,3 +1,6 @@
 # WRITE YOUR FUNCTIONS HERE
 def get_pet_shop_name(shop_details):
     return shop_details["name"]
+
+def get_total_cash(shop_details):
+    return shop_details["admin"]["total_cash"]
